f7750eacb0a8d9209b2d52a27af977df1e9b6c37
Disconnected59 <bekir59100@hotmail.fr>
2018-02-19 14:13:26 +0100

    gh

--- js/pdfFromHTML.js
+++ js/pdfFromHTML.js
@@ -1,3 +1,4 @@
+//fonction pour transformer en pdf
 function HTMLtoPDF(){
 var pdf = new jsPDF('p', 'pt', 'letter');
 source = $('#HTMLtoPDF')[0];
@@ -12,16 +13,14 @@ margins = {
     width: 545
   };
 pdf.fromHTML(
-  	source // HTML string or DOM elem ref.
-  	, margins.left // x coord
-  	, margins.top // y coord
+  	source 
+  	, margins.left 
+  	, margins.top 
   	, {
-  		'width': margins.width // max width of content on PDF
+  		'width': margins.width 
   		, 'elementHandlers': specialElementHandlers
   	},
   	function (dispose) {
-  	  // dispose: object with X, Y of the last line add to the PDF
-  	  //          this allow the insertion of new lines after html
         pdf.save('Stefane-Plagiat.pdf');
       }
   )		
