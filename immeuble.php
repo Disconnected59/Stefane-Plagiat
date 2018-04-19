ad26cac4daacabc2bff069c9f034897dd02cb4ea
Disconnected59 <bekir59100@hotmail.fr>
2018-04-19 15:59:26 +0200

    hjg

--- immeuble.php
+++ immeuble.php
@@ -17,7 +17,7 @@ $objetPDO= new PDO('mysql:host=localhost;dbname=bddstefaneplagiat','root','');
                <fieldset id="connexion">
                    <?php
 include_once 'FonctionsPhp/fonctionsBackOffice.php';
-$objetPDO= new PDO('mysql:host=localhost;dbname=bddstefaneplagiat','root','');
+$PDO= new PDO('mysql:host=localhost;dbname=bddstefaneplagiat','root','');
 
 if(isset($_POST['login']))
 {
@@ -62,15 +62,103 @@ else
 <?php 
  include_once'/include/menuEtImage.inc';
 ?>
-            
-            
+            <br/><br/><br/>
+                
+            <form method="post" action="#">
+            <div class="recherche">
+                <div>
+                    Type de produit:
+                    <?php
+                    echo("<div><input type='radio' name='Type' value='tous' id='tousType' checked/> <label for='tousType'>Tous</label></div>");
+                    $Type = getTypeBien($PDO);
+                    $i=1;
+                    foreach ($Type as $value) {
+                        echo("<div><input type='radio' name='Type' value=".$i." id=".$value['libelle']." /> <label for=".$value['libelle'].">".$value['libelle']."</label></div>");
+                        $i++;
+                    }
+                    ?>
+                 </div>
+                 <div class="marge">
+                     Jardin:
+                     <div><input type="radio" name="Jardin" value="tous" id="tousJardin" checked/> <label for="tousJardin">Tous</label></div>
+                     <div><input type="radio" name="Jardin" value="Oui" id="oui" /> <label for="oui">Oui</label></div>
+                     <div><input type="radio" name="Jardin" value="Non" id="non" /> <label for="non">Non</label></div>
+                 </div>
+                 <div class="marge">
+                     Nombre de pièces:
+                     <?php
+                     echo("<div><input type='radio' name='Piece' value='tous' id='tousPiece' checked/> <label for='tousPiece'>Tous</label></div>");
+                     $piece = getPiece($PDO);
+                     foreach ($piece as $value) {
+                         echo("<div><input type='radio' name='Piece' value=".$value['piece']." id=".$value['piece']." /> <label for=".$value['piece'].">".($value['piece']-10)." à ".$value['piece']."</label></div>");
+                     }
+                     echo("<div><input type='radio' name='Piece' value=".($value['piece']+1)." id=".($value['piece']+1)." /> <label for=".$value['piece'].">".($value['piece']+1)." et plus</label></div>");
+                     ?>
+                 </div>
+                 <div class="marge">
+                     Surface:
+                     <?php
+                     echo("<div><input type='radio' name='surface' value='tous' id='tousSurface' checked/> <label for='tousSurface'>Tous</label></div>");
+                     $surface = getSurface($PDO);
+                     foreach ($surface as $value) {
+                         echo("<div><input type='radio' name='surface' value=".$value['surface']." id=".$value['surface']." /> <label for=".$value['surface'].">".($value['surface']-200)." à ".$value['surface']."</label></div>");
+                     }
+                     echo("<div><input type='radio' name='surface' value=".($value['surface']+1)." id=".($value['surface']+1)." /> <label for=".$value['surface'].">".($value['surface']+1)." et plus</label></div>");
+                     ?>
+
+                 </div>
+                 <div class="marge">
+                     Prix (en €):
+                     <?php
+                     echo("<div><input type='radio' name='prix' value='tous' id='tousPrix' checked/> <label for='tousPrix'>Tous</label></div>");
+                     $prix = getPrix($PDO);
+                     foreach ($prix as $value) {
+                         echo("<div><input type='radio' name='prix' value=".$value['prix']." id=".$value['prix']." /> <label for=".$value['prix'].">".($value['prix']-200000)." à ".$value['prix']."</label></div>");
+                     }
+                     echo("<div><input type='radio' name='prix' value=".($value['prix']+1)." id=".($value['prix']+1)." /> <label for=".$value['prix'].">".($value['prix']+1)." et plus</label></div>");
+                     ?>
+                 </div>
+                 <div class="marge">
+                     location:<br/>
+                     <select name="location">
+                         <?php
+                         $ville = getVille($PDO);
+                         var_dump($ville);
+                         echo "<option value='tous'>Tous</option>";
+                         foreach ($ville as $value) {
+                             echo "<option value='".$value['ville']."'>".$value['ville']."</option>";
+                         }
+                         ?>
+                     </select>
+                 </div>
+                 <div class="marge">
+                     <input type="submit" value="Rechercher"/>
+                     <input type="reset" value="mise à zero"/>
+                 </div>
+           </div>
+        </form>
             
 	<div class="titre">
 	<h1> Immeubles en vente </h1>
 	</div>
 			<div class="contoursmaison">
 			<?php
-                        $mesMaisons=getInfosImmeubles($objetPDO);
+                        if (isset($_POST['location'])) {
+                $lesMaisons= selectionBien($PDO, $_POST['Type'], $_POST['Jardin'], $_POST['Piece'], $_POST['surface'], $_POST['prix'], $_POST['location']);
+                foreach ($lesMaisons as $maMaison){
+                echo '<div class="contoursmaison">';
+                            echo "<h2>".$maMaison['titre']."</h2>";
+                            echo '<p> <img src="'.$maMaison['image'].'" width="300" height="auto" alt="Image de la maison">';
+                            echo $maMaison['description']." </p>";
+                    echo '<p class="prix"> '.$maMaison['prix'].'€ </p>';
+                    echo '<div align="right">';
+                    echo '<a href="maison1.php?id='.$maMaison['numero'].'"><img src="http://upload.dinhosting.fr/x/6/p/voirledetail.PNG" alt="voir le détail"/></a>';
+                    echo '</div>';
+                    echo '</div>';
+                }
+            }
+            else{
+                        $mesMaisons=getInfosImmeubles($PDO);
                         foreach ($mesMaisons as $maMaison){
                         echo '<div class="contoursmaison">';
 				echo "<h2>".$maMaison['titre']."</h2>";
@@ -83,6 +171,7 @@ else
                         
 			echo '</div>';
                         }
+            }
                         ?>
 			
 			
