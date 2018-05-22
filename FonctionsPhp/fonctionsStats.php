73a84a5a1a786f6759be5a69398d329b249cfdbd
Nullos59 <35691900+Nullos59@users.noreply.github.com>
2018-05-22 12:04:13 +0200

    ghdrgr

--- FonctionsPhp/fonctionsStats.php
+++ FonctionsPhp/fonctionsStats.php
@@ -1,8 +1,10 @@
 <?php
 
-function donneVilleParNombreRecherche($PDO)
+function donneVilleParNombreRecherche($PDO, $dateDebut, $dateFin)
 {
-    $PDO = $PDO->prepare("SELECT distinct ville, count(ville) as cVille FROM stats group by ville");
+    $PDO = $PDO->prepare("SELECT distinct ville, count(ville) as cVille FROM stats group by ville where dateRecherche between :dateDebut and :dateFin");
+    $Test0 = $PDO->bindValue(':dateDebut',$dateDebut);
+    $Test1 = $PDO->bindValue(':dateFin',$dateFin);
     $resultat = $PDO->execute();
     $resultat = $PDO->fetchall();
     $PDO-> closeCursor();
@@ -13,7 +15,9 @@ function donneVilleParNombreRecherche($PDO)
 
 function donnePrixParNombreRecherche($PDO)
 {
-    $PDO = $PDO->prepare("SELECT distinct prix, count(prix) as cPrix FROM stats group by prix");
+    $PDO = $PDO->prepare("SELECT distinct prix, count(prix) as cPrix FROM stats group by prix where dateRecherche between ".$dateDebut." and ".$dateFin.";");
+    $Test0 = $PDO->bindValue(':dateDebut',$dateDebut);
+    $Test1 = $PDO->bindValue(':dateFin',$dateFin);
     $resultat = $PDO->execute();
     $resultat = $PDO->fetchall();
     $PDO-> closeCursor();
@@ -24,7 +28,9 @@ function donnePrixParNombreRecherche($PDO)
 
 function donneSurfaceParNombreRecherche($PDO)
 {
-    $PDO = $PDO->prepare("SELECT distinct surface, count(surface) as cSurface FROM stats group by surface");
+    $PDO = $PDO->prepare("SELECT distinct surface, count(surface) as cSurface FROM stats group by surface where dateRecherche between ".$dateDebut." and ".$dateFin.";");
+    $Test0 = $PDO->bindValue(':dateDebut',$dateDebut);
+    $Test1 = $PDO->bindValue(':dateFin',$dateFin);
     $resultat = $PDO->execute();
     $resultat = $PDO->fetchall();
     $PDO-> closeCursor();
